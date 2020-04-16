2df8dc3072fa707d9d548314cab6b646a2641e9e
Sam-Glerum <samuelglerum@hotmail.com>
2020-04-16 13:13:08 +0200

    added routing for game_result

--- wsip-frontend-angular/src/app/app-routing.module.ts
+++ wsip-frontend-angular/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomepageComponent} from "./homepage/homepage.component";
 import {AppComponent} from "./app.component";
+import {GameResultComponent} from "./game-result/game-result.component";
 
 
 const routes: Routes = [
-
+  {path: '', component: HomepageComponent},
+  {path: 'game_result', component: GameResultComponent}
 ];
 
 @NgModule({
